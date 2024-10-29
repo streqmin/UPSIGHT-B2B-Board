047f8a108603fe6fd1a84f31495394096a62a99c
ssangttuce <sanghoo1023@gmail.com>
2024-10-30 01:38:40 +0900

    #10 make  CookieJWTAuthenticate overides 'authenticate' method instead of 'get_raw_token' method

--- authentication/AuthCookieJWT.py
+++ authentication/AuthCookieJWT.py
@@ -1,6 +1,12 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CookieJWTAuthentication(JWTAuthentication):
-    def get_raw_token(self, request):
-        # 기본적으로 Authorization 헤더에서 토큰을 찾지만, 여기서는 쿠키에서 토큰을 찾음
-        return request.COOKIES.get('access_token')
+    def authenticate(self, request):
+        # 쿠키에서 access_token을 가져옴
+        raw_token = request.COOKIES.get('access_token')
+        if raw_token is None:
+            return None  # 토큰이 없으면 None을 반환하여 인증을 수행하지 않음
+        
+        # 부모 클래스의 메소드를 사용하여 검증
+        validated_token = self.get_validated_token(raw_token)
+        return self.get_user(validated_token), validated_token
