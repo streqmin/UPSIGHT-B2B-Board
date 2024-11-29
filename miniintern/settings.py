04f2a1d51b8f90f6c29643bf3a71d05c0ecf34f9
ssangttuce <sanghoo1023@gmail.com>
2024-11-29 16:16:43 +0900

    view for post do ordering by created time

--- miniintern/settings.py
+++ miniintern/settings.py
@@ -89,7 +89,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        ],
 }
 
 SWAGGER_SETTINGS = {
