8cd5400fc7c9dae02333f6cadac1178b5c1e7149
김상후 <sanghoo1023@gmail.com>
2024-12-07 15:56:34 +0900

    #44 simpleJWT settings for logout

--- miniintern/settings.py
+++ miniintern/settings.py
@@ -121,8 +121,8 @@ from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
     'VERIFYING_KEY': None,
