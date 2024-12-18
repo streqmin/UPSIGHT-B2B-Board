77a7f5954d2ee860c2b20387615a9ac3d4863979
김상후 <sanghoo1023@gmail.com>
2024-12-18 17:06:56 +0900

    server sends specific message for expiration of JWT

--- authentication/backends.py
+++ authentication/backends.py
@@ -1,24 +1,30 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework.exceptions import AuthenticationFailed
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        access_token = request.COOKIES.get('access_token')
+        access_token = request.COOKIES.get("access_token")
 
         if not access_token:
-            return None
+            return None  # 토큰 없음
 
         try:
             validated_token = self.get_validated_token(access_token)
-            user = self.get_user(validated_token)
         except InvalidToken as e:
-            raise AuthenticationFailed("Invalid token.")
-        except TokenError as e:
-            raise AuthenticationFailed("Token error: " + str(e))
-        except Exception as e:
-            raise AuthenticationFailed(str(e))
+            # InvalidToken의 상세 메시지 분석
+            error_detail = getattr(e, "detail", None)
+            if error_detail and isinstance(error_detail, dict):
+                messages = error_detail.get("messages", [])
+                for msg in messages:
+                    # 만료된 토큰인지 확인
+                    if "만료된" in str(msg.get("message", "")) or "expired" in str(msg.get("message", "")):
+                        raise AuthenticationFailed("Access token has expired.")  # 만료 메시지 반환
+            raise AuthenticationFailed("Invalid token.")  # 기본 메시지
+        except TokenError:
+            raise AuthenticationFailed("Token error.")  # 기타 토큰 에러
 
+        user = self.get_user(validated_token)
         if user is None or not user.is_active:
             raise AuthenticationFailed("User is inactive.")
 
