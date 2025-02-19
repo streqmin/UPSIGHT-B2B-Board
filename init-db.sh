fff662a1d6a448605d9b88840584d7379238b512
ssangttuce <sanghoo1023@gmail.com>
2025-02-19 10:41:45 +0900

    init-db.sh creates database after Postgresql is ready

--- init-db.sh
+++ init-db.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 set -e
 
-psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
-    -- 사용자 생성 (존재하지 않을 경우)
+echo "Initializing database if needed..."
+
+COUNT_TIME=0
+until pg_isready -U $POSTGRES_USER; do
+  echo "Waiting for PostgreSQL to start...$COUNT_TIME"
+  sleep 1
+  COUNT_TIME=$((COUNT_TIME + 1))
+done
+
+# psql 내부에서 직접 SQL 실행
+psql -U $POSTGRES_USER <<-EOSQL
     DO \$\$
     BEGIN
-        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$POSTGRES_USER') THEN
-            CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';
+        -- 데이터베이스 생성 (존재하지 않을 경우)
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${POSTGRES_DB}') THEN
+            CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER};
+            GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
         END IF;
     END
     \$\$;
+EOSQL
 
-    -- 데이터베이스 생성 (존재하지 않을 경우)
-    SELECT 'CREATE DATABASE $POSTGRES_DB'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$POSTGRES_DB')\gexec
+echo "Database initialization completed."
